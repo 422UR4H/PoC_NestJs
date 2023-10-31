61fdf98331ffd9cafa5ad6b19bf81e1e2b11ba16
422UR4H <tiagosindra@id.uff.br>
2023-10-31 17:40:27 -0300

    :recycle: refactor: controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,29 +1,29 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { User } from './entities/user.entity';
-import { CreateUserDto } from './dtos/user.dto';
+import { CreateUserDTO } from './dtos/user.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
+  getHealth(): string {
     return this.appService.getHealth();
   }
 
   @Get('users')
-  getUsers(): User[] {
-    return this.appService.getUsers();
+  findUsers(): User[] {
+    return this.appService.findUsers();
   }
 
   @Get('users/:nick') // se fosse um id, usaríamos @Param('id', ParseIntPipe) id: number
-  getUserByNick(@Param('nick') nick: string): User | undefined {
-    return this.appService.getUserByNick(nick);
+  findUserByNick(@Param('nick') nick: string): User | undefined {
+    return this.appService.findUserByNick(nick);
   }
 
-  @Post('users')
-  postUserByNick(@Body() body: CreateUserDto): void {
-    return this.appService.postUser(body);
+  @Post('sign-up')
+  signUp(@Body() body: CreateUserDTO): void {
+    return this.appService.signUp(body);
   }
 }
